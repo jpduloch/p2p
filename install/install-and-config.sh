ccd60709a7dca469d7e912a10e882518c8a4249a
jpduloch <roland.mosimann@students.fhnw.ch>
2017-05-19 21:42:22 +0200

    removed unused folder

--- install/install-and-config.sh
+++ install/install-and-config.sh
@@ -89,8 +89,3 @@ EOF
 PS1='\\n\\[\\033[01;32m\\]${debian_chroot:+($debian_chroot)}\\[\\033[00m\\]\\u@\\h\\[\\033[00m\\]:\\[\\033[01;34m\\]\\w\\[\\e[32m\\]$(parse_git_branch)\\n==> \\$ \\[\\033[00m\\]'
 sed -i /root/.bashrc \
     -e "/^if \[ \"\$color_prompt\" = yes \]/,+2 s/PS1=.*/PS1='$PS1'/"
-
-
-### Create folder for STUN exchange
-mkdir /home/vnc/stun_dumps
-chmod 0777 /home/vnc/stun_dumps
